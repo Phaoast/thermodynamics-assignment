3398560710df9c1e172eadfaedaaf24d0776cfec
Anthony Danella <78374496+Phaoast@users.noreply.github.com>
2024-10-03 14:04:29 +0200

    Format

--- Assignment.m
+++ Assignment.m
@@ -1,21 +1,21 @@
 clear all;close all;clc;
 warning off
-%% To make sure that matlab will find the functions. You must change it to your situation 
+%% To make sure that matlab will find the functions. You must change it to your situation
 relativepath_to_generalfolder='General'; % relative reference to General folder (assumes the folder is in you working folder)
-addpath(relativepath_to_generalfolder); 
+addpath(relativepath_to_generalfolder);
 %% Load Nasadatabase
 TdataBase=fullfile('General','NasaThermalDatabase');
 load(TdataBase);
-%% Nasa polynomials are loaded and globals are set. 
-%% values should not be changed. These are used by all Nasa Functions. 
+%% Nasa polynomials are loaded and globals are set.
+%% values should not be changed. These are used by all Nasa Functions.
 global Runiv Pref
 Runiv=8.314472;
 Pref=1.01235e5; % Reference pressure, 1 atm!
 Tref=298.15;    % Reference Temperature
 %% Some convenient units
 kJ=1e3;kmol=1e3;dm=0.1;bara=1e5;kPa = 1000;kN=1000;kg=1;s=1;
-%% Given conditions. 
-%  For the final assignment take the ones from the specific case you are supposed to do.                  
+%% Given conditions.
+%  For the final assignment take the ones from the specific case you are supposed to do.
 v1=200;Tamb=250;P3overP2=7;Pamb=45*kPa;mfurate=0.68*kg/s;AF=75;             % These are the ones from the book
 cFuel='Gasoline';                                                           % Pick Gasoline as the fuel (other choices check Sp.Name)
 %% Select species for the case at hand
@@ -25,17 +25,17 @@ NSp = length(SpS);
 Mi = [SpS.Mass];
 %% Air composition
 Xair = [0 0.21 0 0 0.79];                                                   % Order is important. Note that these are molefractions
-MAir = Xair*Mi';                                                            % Row times Column = inner product 
+MAir = Xair*Mi';                                                            % Row times Column = inner product
 Yair = Xair.*Mi/MAir;                                                       % Vector. times vector is Matlab's way of making an elementwise multiplication
 %% Fuel composition
 Yfuel = [1 0 0 0 0];                                                        % Only fuel
 %% Range of enthalpies/thermal part of entropy of species
 TR = [200:1:3000];NTR=length(TR);
-for i=1:NSp                                                                 % Compute properties for all species for temperature range TR 
+for i=1:NSp                                                                 % Compute properties for all species for temperature range TR
     hia(:,i) = HNasa(TR,SpS(i));                                            % hia is a NTR by 5 matrix
     sia(:,i) = SNasa(TR,SpS(i));                                            % sia is a NTR by 5 matrix
 end
-hair_a= Yair*hia';                                                          % Matlab 'inner product': 1x5 times 5xNTR matrix muliplication, 1xNTR resulT -> enthalpy of air for range of T 
+hair_a= Yair*hia';                                                          % Matlab 'inner product': 1x5 times 5xNTR matrix muliplication, 1xNTR resulT -> enthalpy of air for range of T
 sair_a= Yair*sia';                                                          % same but this thermal part of entropy of air for range of T
 % whos hia sia hair_a sair_a                                                  % Shows dimensions of arrays on commandline
 %% Two methods are presented to 'solve' the conservation equations for the Diffusor
